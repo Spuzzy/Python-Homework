3373993b8abd89e48284857e7008c7864a00ed21
Spuzzy <spuzzy.buzzy@gmail.com>
2020-04-16 22:07:21 +0800

    First upload in 2020

---
+++ ex3.py
@@ -0,0 +1,42 @@
+
+# Output: <class 'float'>
+
+# prints I will now count my chickens:
+print("I will now count my chickens:")
+
+# prints Hens, does calculation using order of operations
+print("Hens", float(25 + 30 / 6))
+# prints Roosters, does calculation using order of operations
+print("Roosters", float(100 - 25 * 3 % 4))
+
+# prints Now I will count the eggs:
+print("Now I will count the eggs:")
+
+# does calculation using order of operations
+# # = modulus - divides left hand operand by right hand operand and returns remainder
+print(float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6))
+
+# prints Is it true that 3 + 2 < 5 - 7?
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# prints logic true or false ?
+print(3 + 2 < 5 - 7)
+
+# prints string followed by numerical answer
+print("What is 3 + 2?", float(3 + 2))
+# prints string followed by numerical answer
+print("What is 5 - 7?", float(5 - 7))
+
+# prints string
+print("Oh, that's why it's False.")
+
+# prints string
+print("How about some more.")
+
+# prints string followed by true or false
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+# print pi
+print(float(22/7))
---
+++ ex5.py
@@ -0,0 +1,20 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+inchesheight = 74 # inches
+poundsweight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+cmheight = inchesheight * 2.54
+kgweight = poundsweight * 0.453592
+
+print(f"Let's talk about {name}.")
+print(f"He's {inchesheight} inches or {cmheight} cm tall.")
+print(f"He's {poundsweight} pounds or {round(kgweight,2)} kg heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usuall {teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = age + inchesheight + poundsweight
+print(f"If I add {age}, {inchesheight}, and {poundsweight} I get {total}.")
